76de40e066e8affd4a776ff6d02d627942d1700e
clementrayroud <clementrayroud@users.noreply.github.com>
2022-04-29 13:29:49 +0000

    deploy: 6c0981d1a051a98c1e604648413fde51d2dfb696

---
+++ _nuxt/static/1651238983/info/l-attaque-des-clones/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fstar-wars\u002F_nuxt\u002Fstatic\u002F1651238983",layout:"default",error:a,serverRendered:true,routePath:"\u002Finfo\u002Fl-attaque-des-clones",config:{_app:{basePath:"\u002Fstar-wars\u002F",assetsPath:"\u002Fstar-wars\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"215624b7"}}}}(null));
