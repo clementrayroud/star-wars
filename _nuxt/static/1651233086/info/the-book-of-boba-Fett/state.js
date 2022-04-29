714b621d554936e9b5e31db4149f9eb7d9e77c7e
clementrayroud <clementrayroud@users.noreply.github.com>
2022-04-29 11:56:20 +0000

    deploy: 0ceaaab384c38afd5b010daa711ab4939594d535

--- _nuxt/static/1651233086/info/the-book-of-boba-Fett/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fstar-wars\u002F_nuxt\u002Fstatic\u002F1651233086",layout:"default",error:a,serverRendered:true,routePath:"\u002Finfo\u002Fthe-book-of-boba-Fett",config:{_app:{basePath:"\u002Fstar-wars\u002F",assetsPath:"\u002Fstar-wars\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f6b42db3"}}}}(null));
