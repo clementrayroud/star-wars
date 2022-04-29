94d9aeaa19162f67116feae047528671bede5227
clementrayroud <clementrayroud@users.noreply.github.com>
2022-04-29 12:42:13 +0000

    deploy: 2d5cac6bd76a87ac1bbaf73e0148a5ebf4881147

---
+++ _nuxt/static/1651236127/info/l-ascension-de-skywalker/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fstar-wars\u002F_nuxt\u002Fstatic\u002F1651236127",layout:"default",error:a,serverRendered:true,routePath:"\u002Finfo\u002Fl-ascension-de-skywalker",config:{_app:{basePath:"\u002Fstar-wars\u002F",assetsPath:"\u002Fstar-wars\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"226ad5dc"}}}}(null));
