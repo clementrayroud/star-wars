836a82ff17f1359013edcd4cb385c792fa2de98b
clementrayroud <clementrayroud@users.noreply.github.com>
2022-04-29 12:02:43 +0000

    deploy: 0bc03293b88b40e437af9414177a78febe1b7797

--- _nuxt/static/1651233680/info/star-wars-rebels/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fstar-wars\u002F_nuxt\u002Fstatic\u002F1651233680",layout:"default",error:a,serverRendered:true,routePath:"\u002Finfo\u002Fstar-wars-rebels",config:{_app:{basePath:"\u002Fstar-wars\u002F",assetsPath:"\u002Fstar-wars\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"61e8b444"}}}}(null));
---
+++ _nuxt/static/1651233758/info/l-empire-contre-attaque/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fstar-wars\u002F_nuxt\u002Fstatic\u002F1651233758",layout:"default",error:a,serverRendered:true,routePath:"\u002Finfo\u002Fl-empire-contre-attaque",config:{_app:{basePath:"\u002Fstar-wars\u002F",assetsPath:"\u002Fstar-wars\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a519bb16"}}}}(null));
