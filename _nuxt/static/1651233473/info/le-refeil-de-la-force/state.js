877b2dd5f4cf3df845dbf4c7052fde16b6a61ec8
clementrayroud <clementrayroud@users.noreply.github.com>
2022-04-29 11:59:03 +0000

    deploy: 8bf4bcf5642259dbf3f512f8cd6a093b49bb8091

--- _nuxt/static/1651233473/info/le-refeil-de-la-force/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fstar-wars\u002F_nuxt\u002Fstatic\u002F1651233473",layout:"default",error:a,serverRendered:true,routePath:"\u002Finfo\u002Fle-refeil-de-la-force",config:{_app:{basePath:"\u002Fstar-wars\u002F",assetsPath:"\u002Fstar-wars\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e918c326"}}}}(null));
