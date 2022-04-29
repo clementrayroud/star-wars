3012e43bfd77a08f28dcdb175f3a140ec499cdcf
clementrayroud <clementrayroud@users.noreply.github.com>
2022-04-29 13:47:54 +0000

    deploy: 087bca3a65f6de7232743b8e2a5eea5817721ca4

--- _nuxt/static/1651239847/info/the-clone-wars/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fstar-wars\u002F_nuxt\u002Fstatic\u002F1651239847",layout:"default",error:a,serverRendered:true,routePath:"\u002Finfo\u002Fthe-clone-wars",config:{_app:{basePath:"\u002Fstar-wars\u002F",assetsPath:"\u002Fstar-wars\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5664de42"}}}}(null));
