17c2b91f6682a1551beb0f83d32f1a3f1d478a9b
clementrayroud <clementrayroud@users.noreply.github.com>
2022-04-29 08:34:21 +0000

    deploy: bc4ca83ce32f5d8078d2d7e9b2edb6a9530b78e0

--- _nuxt/static/1651220743/info/un-nouvel-espoir/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fstar-wars\u002F_nuxt\u002Fstatic\u002F1651220743",layout:"default",error:a,serverRendered:true,routePath:"\u002Finfo\u002Fun-nouvel-espoir",config:{_app:{basePath:"\u002Fstar-wars\u002F",assetsPath:"\u002Fstar-wars\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c014e7e1"}}}}(null));
