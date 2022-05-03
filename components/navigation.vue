b9ae4c42c2d2df8d224251b3a5159d3e556d29f3
Clément <rayroudclement@gmail.com>
2022-05-03 13:02:10 +0200

    supretion

--- components/navigation.vue
+++ components/navigation.vue
@@ -10,20 +10,7 @@
         <b-nav-item href="/star-wars/">accueil</b-nav-item>
         <b-nav-item href="/star-wars/film">films et serie</b-nav-item>
       </b-navbar-nav>
-
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-        <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder="recherche"></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-        </b-nav-form>
-
-        <b-nav-item-dropdown text="lang" right>
-          <b-dropdown-item href="#">englais</b-dropdown-item>
-          <b-dropdown-item href="#">français</b-dropdown-item>
-        </b-nav-item-dropdown>
-
-        
+    
         </b-nav-item-dropdown>
       </b-navbar-nav>
     </b-collapse>
