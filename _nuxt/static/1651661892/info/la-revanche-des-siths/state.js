1800b921169bb3e411eb64956f18c7d99a45d2d9
clementrayroud <clementrayroud@users.noreply.github.com>
2022-05-04 10:58:17 +0000

    deploy: c66595b647571942644708e2b782f03ab34cad3f

---
+++ _nuxt/static/1651661892/info/la-revanche-des-siths/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fstar-wars\u002F_nuxt\u002Fstatic\u002F1651661892",layout:"default",error:a,serverRendered:true,routePath:"\u002Finfo\u002Fla-revanche-des-siths",config:{_app:{basePath:"\u002Fstar-wars\u002F",assetsPath:"\u002Fstar-wars\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"57f97b0c"}}}}(null));
