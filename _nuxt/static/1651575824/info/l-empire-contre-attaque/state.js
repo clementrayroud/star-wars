07fbc3de55b4c82855a7aa429ede6180cc65e059
clementrayroud <clementrayroud@users.noreply.github.com>
2022-05-04 10:56:07 +0000

    deploy: a7164f8d48d03d66bfc134ebc59e1630f233341b

--- _nuxt/static/1651575824/info/l-empire-contre-attaque/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fstar-wars\u002F_nuxt\u002Fstatic\u002F1651575824",layout:"default",error:a,serverRendered:true,routePath:"\u002Finfo\u002Fl-empire-contre-attaque",config:{_app:{basePath:"\u002Fstar-wars\u002F",assetsPath:"\u002Fstar-wars\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c0025820"}}}}(null));
